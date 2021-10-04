484b2efc3a3e491dd23fb7e912771bbd381696e0
Lucas Bonilla <lucas@moinhosul.com.br>
2021-10-04 00:54:42 -0300

    feat(upload): cerraga arquivo e faz a limpeza dos dados

--- services/web/project/frontend/src/components/HelloWorld.vue
+++ services/web/project/frontend/src/components/HelloWorld.vue
@@ -1,41 +1,56 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>Upload file
+        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+      </label>
+        <button v-on:click="submitFile()">Submit</button>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
+
+  data(){
+    return {
+      file: ''
+    }
+  },
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  methods: {
+    handleFileUpload(){
+      this.file = this.$refs.file.files[0];
+    },
+    submitFile(){
+      const axios = require('axios');
+      let formData = new FormData();
+      formData.append('file', this.file);
+      axios({
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        method: "post",
+        url: "http://localhost:5000/upload-file",
+        data: formData
+      }).then(res => {
+        console.log(res);
+      }).catch(error => {
+        console.log(error)
+        if (!error.response) {
+          // network error
+          this.errorStatus = "Error: Network Error";
+        } else {
+          this.errorStatus = error.response.data.message;
+        }
+      });
+    },
+  },
+  
 }
 </script>
 
